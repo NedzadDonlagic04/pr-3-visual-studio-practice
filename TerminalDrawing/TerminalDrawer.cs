196dd4d2286df393c39bbbe4c7b33c4c14eac1e5
NedzadDonlagic04 <nedzaddonlagic04@gmail.com>
2024-08-03 22:56:57 +0200

    Added EnterDrawingFileName

--- TerminalDrawing/TerminalDrawer.cs
+++ TerminalDrawing/TerminalDrawer.cs
@@ -1,28 +1,47 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TerminalDrawing
-{
+﻿namespace TerminalDrawing
+{   
     internal class TerminalDrawer
     {
-        private const string defaultFileName = "defaultFileName";
+        private const string DrawingDirectoryPath = "./terminalDrawingOutput";
+        private const string DefaultFileName = "defaultFileName";
+        private string _fileName;
+        private string FilePath => $"{DrawingDirectoryPath}/{_fileName}.txt";
+
+        public TerminalDrawer() 
+        {
+            _fileName = DefaultFileName;
+        }
 
         public void Start() {
             PrintWelcomeMessage();
-
-            Console.Write("Enter the name of the .txt file to which your work will be saved: ");
-            string? filePath = Console.ReadLine() ?? defaultFileName;
-
-            Console.WriteLine($"Entered file path {filePath}");
-        }
+            EnterDrawingFileName();
+            StartDrawingProcess();
+        }   
 
         private static void PrintWelcomeMessage() {
             // Possibly expand welcome message as I have no idea
             // what more I could say to my imaginary friend testing this
             Console.WriteLine("Welcome to \"Termina Drawer\" an excersize to help me better understand C#");
+        }
+
+        private void EnterDrawingFileName()
+        {
+            char option = 'n';
+            do {
+                Console.Write("Enter the name of file where the drawing will be saved: ");
+                _fileName = Console.ReadLine() ?? DefaultFileName;
+
+                if (File.Exists(FilePath)) {
+                    Console.WriteLine("The entered name already exists, which means saving will override what is currently inside the file");
+                    Console.Write("Do you wish to change the file name(y\\n): ");
+                    option = char.Parse(Console.ReadLine() ?? "n");
+                    Console.WriteLine();
+                }
+            } while (option != 'n');
+        }
+
+        private void StartDrawingProcess() {
+            Console.WriteLine("StartDrawingProcess");
         }
     }
 }
