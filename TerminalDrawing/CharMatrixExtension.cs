f615d6dd92f89050b5b1180e1c2fd0eb832eefa1
NedzadDonlagic04 <nedzaddonlagic04@gmail.com>
2024-08-05 00:50:46 +0200

    Extended char matrix with Fill method

---
+++ TerminalDrawing/CharMatrixExtension.cs
@@ -0,0 +1,15 @@
+﻿namespace CharMatrixExtensions
+{
+    public static class CharMatrixExtensions
+    {
+        public static char[,] Fill(this char[,] matrix, char value) {
+            for (int i = 0; i < matrix.GetLength(0); ++i) {
+                for (int ii = 0; ii < matrix.GetLength(1); ++ii) {
+                    matrix[i, ii] = value;
+                }
+            }
+
+            return matrix;
+        }
+    }
+}
