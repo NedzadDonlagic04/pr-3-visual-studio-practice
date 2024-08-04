8e065bcb03621df8df1a870d6d207451131d7cbb
NedzadDonlagic04 <nedzaddonlagic04@gmail.com>
2024-08-05 00:51:29 +0200

    Added TerminalCanvas field and logic surrounding input

--- TerminalDrawing/TerminalDrawer.cs
+++ TerminalDrawing/TerminalDrawer.cs
@@ -1,15 +1,25 @@
-﻿namespace TerminalDrawing
+﻿using System.Drawing;
+using System.Security.Cryptography;
+
+namespace TerminalDrawing
 {   
     internal class TerminalDrawer
     {
+        private const int CanvasHeight = 15;
+        private const int CanvasWidth = CanvasHeight * 2;
+
         private const string DrawingDirectoryPath = "./terminalDrawingOutput";
         private const string DefaultFileName = "defaultFileName";
         private string _fileName;
         private string FilePath => $"{DrawingDirectoryPath}/{_fileName}.txt";
 
+        private readonly TerminalCanvas _terminalCanvas;
+
         public TerminalDrawer() 
         {
             _fileName = DefaultFileName;
+            _terminalCanvas = new(CanvasWidth, CanvasHeight);
+
         }
 
         public void Start() {
@@ -27,6 +37,7 @@
         private void EnterDrawingFileName()
         {
             char option = 'n';
+
             do {
                 Console.Write("Enter the name of file where the drawing will be saved: ");
                 _fileName = Console.ReadLine() ?? DefaultFileName;
@@ -41,7 +52,55 @@
         }
 
         private void StartDrawingProcess() {
-            Console.WriteLine("StartDrawingProcess");
+            ConsoleKeyInfo enteredKeyInfo;
+
+            var cursorPosition = new Point(0, 0);
+            char keyChar = 'A';
+
+            Console.Clear();
+            
+            do
+            {
+                Console.CursorVisible = false;
+                Console.SetCursorPosition(0, 0);
+                
+                _terminalCanvas.PrintCanvas();
+                
+                Console.SetCursorPosition(cursorPosition.X + 1, cursorPosition.Y + 1);
+                Console.Write(keyChar);
+                Console.SetCursorPosition(cursorPosition.X + 1, cursorPosition.Y + 1);
+                Console.CursorVisible = true;
+
+                enteredKeyInfo = Console.ReadKey(true);
+                HandleKeyInput(enteredKeyInfo, ref cursorPosition, ref keyChar);
+            } while (enteredKeyInfo.Key != ConsoleKey.Escape);
+            
+            Console.Clear();
+            
+            _terminalCanvas.SaveToFile(FilePath);
+        }
+
+        private void HandleKeyInput(ConsoleKeyInfo consoleKeyInfo, ref Point cursorPosition, ref char keyChar) {
+            switch(consoleKeyInfo.Key) {
+                case ConsoleKey.UpArrow:
+                    cursorPosition.Y = Math.Max(cursorPosition.Y - 1, 0);
+                    break;
+                case ConsoleKey.DownArrow: 
+                    cursorPosition.Y = Math.Min(cursorPosition.Y + 1, CanvasHeight - 1);
+                    break;
+                case ConsoleKey.LeftArrow: 
+                    cursorPosition.X = Math.Max(cursorPosition.X - 1, 0);
+                    break;
+                case ConsoleKey.RightArrow: 
+                    cursorPosition.X = Math.Min(cursorPosition.X + 1, CanvasWidth - 1);
+                    break;
+                case ConsoleKey.Enter:
+                    _terminalCanvas.SetPixel(cursorPosition, keyChar);
+                    break;
+                default:
+                    keyChar = consoleKeyInfo.KeyChar;
+                    break;
+            }
         }
     }
 }
