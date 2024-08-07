c32f56d0a585152a7c45cd7ee370701c6f1959b1
NedzadDonlagic04 <nedzaddonlagic04@gmail.com>
2024-08-07 22:13:57 +0200

    Added label with "Todo List" text as well as started experimenting with custom button class

--- WinFormsTodoList/TodoList.Designer.cs
+++ WinFormsTodoList/TodoList.Designer.cs
@@ -28,19 +28,131 @@
         /// </summary>
         private void InitializeComponent()
         {
+            splitContainer = new SplitContainer();
+            todoListActionButton4 = new TodoListActionButton();
+            todoListActionButton3 = new TodoListActionButton();
+            todoListActionButton2 = new TodoListActionButton();
+            todoListActionButton1 = new TodoListActionButton();
+            label1 = new Label();
+            ((System.ComponentModel.ISupportInitialize)splitContainer).BeginInit();
+            splitContainer.Panel1.SuspendLayout();
+            splitContainer.Panel2.SuspendLayout();
+            splitContainer.SuspendLayout();
             SuspendLayout();
+            // 
+            // splitContainer
+            // 
+            splitContainer.Dock = DockStyle.Fill;
+            splitContainer.Location = new Point(0, 0);
+            splitContainer.Name = "splitContainer";
+            // 
+            // splitContainer.Panel1
+            // 
+            splitContainer.Panel1.BackColor = SystemColors.Control;
+            splitContainer.Panel1.Controls.Add(todoListActionButton4);
+            splitContainer.Panel1.Controls.Add(todoListActionButton3);
+            splitContainer.Panel1.Controls.Add(todoListActionButton2);
+            splitContainer.Panel1.Controls.Add(todoListActionButton1);
+            splitContainer.Panel1.Padding = new Padding(20);
+            // 
+            // splitContainer.Panel2
+            // 
+            splitContainer.Panel2.BackColor = SystemColors.Control;
+            splitContainer.Panel2.Controls.Add(label1);
+            splitContainer.Size = new Size(784, 461);
+            splitContainer.SplitterDistance = 261;
+            splitContainer.TabIndex = 0;
+            // 
+            // todoListActionButton4
+            // 
+            todoListActionButton4.Anchor = AnchorStyles.Top;
+            todoListActionButton4.AutoSize = true;
+            todoListActionButton4.Location = new Point(20, 215);
+            todoListActionButton4.Margin = new Padding(20);
+            todoListActionButton4.Name = "todoListActionButton4";
+            todoListActionButton4.Padding = new Padding(20);
+            todoListActionButton4.Size = new Size(221, 65);
+            todoListActionButton4.TabIndex = 3;
+            todoListActionButton4.Text = "todoListActionButton4";
+            todoListActionButton4.UseVisualStyleBackColor = true;
+            // 
+            // todoListActionButton3
+            // 
+            todoListActionButton3.Anchor = AnchorStyles.Top;
+            todoListActionButton3.AutoSize = true;
+            todoListActionButton3.Location = new Point(20, 150);
+            todoListActionButton3.Margin = new Padding(20);
+            todoListActionButton3.Name = "todoListActionButton3";
+            todoListActionButton3.Padding = new Padding(20);
+            todoListActionButton3.Size = new Size(221, 65);
+            todoListActionButton3.TabIndex = 2;
+            todoListActionButton3.Text = "todoListActionButton3";
+            todoListActionButton3.UseVisualStyleBackColor = true;
+            // 
+            // todoListActionButton2
+            // 
+            todoListActionButton2.Anchor = AnchorStyles.Top;
+            todoListActionButton2.AutoSize = true;
+            todoListActionButton2.Location = new Point(20, 85);
+            todoListActionButton2.Margin = new Padding(100);
+            todoListActionButton2.Name = "todoListActionButton2";
+            todoListActionButton2.Padding = new Padding(20);
+            todoListActionButton2.Size = new Size(221, 65);
+            todoListActionButton2.TabIndex = 1;
+            todoListActionButton2.Text = "todoListActionButton2";
+            todoListActionButton2.UseVisualStyleBackColor = true;
+            // 
+            // todoListActionButton1
+            // 
+            todoListActionButton1.Anchor = AnchorStyles.Top;
+            todoListActionButton1.AutoSize = true;
+            todoListActionButton1.Location = new Point(20, 20);
+            todoListActionButton1.Margin = new Padding(20);
+            todoListActionButton1.Name = "todoListActionButton1";
+            todoListActionButton1.Padding = new Padding(20);
+            todoListActionButton1.Size = new Size(221, 65);
+            todoListActionButton1.TabIndex = 0;
+            todoListActionButton1.Text = "todoListActionButton1";
+            todoListActionButton1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 24F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(181, 40);
+            label1.Name = "label1";
+            label1.Size = new Size(156, 45);
+            label1.TabIndex = 0;
+            label1.Text = "Todo List";
             // 
             // TodoList
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            BackColor = SystemColors.ControlDark;
+            ClientSize = new Size(784, 461);
+            Controls.Add(splitContainer);
+            MaximumSize = new Size(900, 500);
+            MinimumSize = new Size(800, 500);
             Name = "TodoList";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "Todo List Application";
+            splitContainer.Panel1.ResumeLayout(false);
+            splitContainer.Panel1.PerformLayout();
+            splitContainer.Panel2.ResumeLayout(false);
+            splitContainer.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)splitContainer).EndInit();
+            splitContainer.ResumeLayout(false);
             ResumeLayout(false);
         }
 
         #endregion
+
+        private SplitContainer splitContainer;
+        private TodoListActionButton todoListActionButton1;
+        private TodoListActionButton todoListActionButton4;
+        private TodoListActionButton todoListActionButton3;
+        private TodoListActionButton todoListActionButton2;
+        private Label label1;
     }
 }
---
+++ WinFormsTodoList/TodoListActionButton.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormsTodoList
+{
+    internal class TodoListActionButton : Button
+    {
+        public TodoListActionButton() { 
+            AutoSize = true;
+
+            Padding = new Padding(20);
+
+            Dock = DockStyle.Top;
+        }
+    }
+}
