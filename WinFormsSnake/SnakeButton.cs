acf5b95a6ed5cc0e6553313e117716a74c17899c
NedzadDonlagic04 <nedzaddonlagic04@gmail.com>
2024-09-09 19:43:06 +0200

    Inherited Button into a custom control since I'll be reusing the design a lot

---
+++ WinFormsSnake/SnakeButton.cs
@@ -0,0 +1,27 @@
+﻿namespace WinFormsSnake
+{
+    public partial class SnakeButton : Button
+    {
+        public SnakeButton()
+        {
+            InitializeComponent();
+            ApplyCustomSettings();
+        }
+
+        private void ApplyCustomSettings() {
+            FlatAppearance.BorderSize = 3;
+            Font = new Font("Segoe UI", 15.75F, FontStyle.Bold, GraphicsUnit.Point);
+            FlatStyle = FlatStyle.Flat;
+            Size = new Size(180, 60);
+            Margin = new Padding(3, 3, 3, 30);
+            
+            BackColor = Color.FromName(Properties.Resources.mainBackColor);
+            ForeColor = Color.FromName(Properties.Resources.mainForeColor);
+        }
+
+        protected override void OnPaint(PaintEventArgs pe)
+        {
+            base.OnPaint(pe);
+        }
+    }
+}
