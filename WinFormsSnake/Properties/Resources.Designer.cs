cd6a566b34b4fda1420faf527bf1bfadf657d9fc
NedzadDonlagic04 <nedzaddonlagic04@gmail.com>
2024-09-09 23:21:09 +0200

    Added game over background image

--- WinFormsSnake/Properties/Resources.Designer.cs
+++ WinFormsSnake/Properties/Resources.Designer.cs
@@ -61,6 +61,16 @@ namespace WinFormsSnake.Properties {
         }
         
         /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap gameOverBackgroundImage {
+            get {
+                object obj = ResourceManager.GetObject("gameOverBackgroundImage", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to GreenYellow.
         /// </summary>
         internal static string mainBackColor {
