2b999c3d3a5d03c6ad828f72780de7ba61e45fbc
NedzadDonlagic04 <nedzaddonlagic04@gmail.com>
2024-09-10 16:44:29 +0200

    Remove background and foreground colors being set

--- WinFormsSnake/SnakeMainMenuForm.cs
+++ WinFormsSnake/SnakeMainMenuForm.cs
@@ -5,15 +5,7 @@ namespace WinFormsSnake
         public SnakeMainMenuForm()
         {
             InitializeComponent();
-            ApplyCustomSettings();
         }
-
-        private void ApplyCustomSettings()
-        {
-            BackColor = Color.FromName(Properties.Resources.mainBackColor);
-            ForeColor = Color.FromName(Properties.Resources.mainForeColor);
-        }
-
         private void QuitGame(object sender, EventArgs e)
         {
             Close();
