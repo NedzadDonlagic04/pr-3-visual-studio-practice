6e241bd440047529b8185b182b12886813232bf4
NedzadDonlagic04 <nedzaddonlagic04@gmail.com>
2024-09-18 01:00:59 +0200

    Extended Point type with Sum method

---
+++ WinFormsSnake/PointExtension.cs
@@ -0,0 +1,10 @@
+﻿namespace Extensions
+{
+    public static class PointExtension
+    {
+        public static Point Sum(this Point lhs, Point rhs)
+        {
+            return new() { X = lhs.X + rhs.X, Y = lhs.Y + rhs.Y };
+        }
+    }
+}
