1200e1d29c13a4bce12b1176c4f246456623cbe5
NedzadDonlagic04 <nedzaddonlagic04@gmail.com>
2024-09-28 22:41:30 +0200

    Added class and struct to manage high scores

---
+++ WinFormsSnake/SnakeHighScoresManager/HighScore.cs
@@ -0,0 +1,37 @@
+﻿namespace WinFormsSnake.SnakeHighScoresManager
+{
+    internal readonly struct HighScore : IComparable
+    {
+        public int Score { get; }
+        public DateTime DateTimeOfScore { get; } = DateTime.Now;
+
+        public HighScore(int score)
+        {
+            Score = score;
+        }
+
+        public override string ToString() => $"Score: {Score} - DateTime: {DateTimeOfScore}";
+
+        public int CompareTo(object? obj)
+        {
+            if (obj == null)
+            {
+                return 1;
+            }
+            else if (obj is not HighScore)
+            {
+                throw new ArgumentException("Object is not a HighScore");
+            }
+
+            HighScore rhs = (HighScore)obj;
+            int scoreCompareRes = Score.CompareTo(rhs.Score);
+
+            if (scoreCompareRes != 0)
+            {
+                return scoreCompareRes;
+            }
+
+            return DateTimeOfScore.CompareTo(rhs.DateTimeOfScore);
+        }
+    }
+}
---
+++ WinFormsSnake/SnakeHighScoresManager/HighScoresManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Text;
+
+namespace WinFormsSnake.SnakeHighScoresManager
+{
+    internal class HighScoresManager
+    {
+        private readonly int _maxHighScores;
+        private readonly SortedSet<HighScore> _highScores = new();
+
+        public SortedSet<HighScore> HighScores { get => _highScores; }
+
+        public HighScoresManager(int maxHighScores)
+        {
+            _maxHighScores = maxHighScores;
+        }
+
+        public void AddScore(int score)
+        {
+            _highScores.Add(new HighScore(score));
+
+            if (_highScores.Count > _maxHighScores)
+            {
+                _highScores.Remove(_highScores.First());
+            }
+        }
+
+        public override string ToString()
+        {
+            StringBuilder stringBuilder = new();
+
+            foreach (HighScore highScore in _highScores)
+            {
+                stringBuilder.AppendLine(highScore.ToString());
+            }
+
+            return stringBuilder.ToString();
+        }
+    }
+}
