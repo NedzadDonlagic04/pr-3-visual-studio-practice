2a2e8aae178f34cc11e2c674cac54fe3c8ee6f8b
NedzadDonlagic04 <nedzaddonlagic04@gmail.com>
2024-10-09 11:13:14 +0200

    Created Token class and TokenType enum

---
+++ BasicMathExpressionParser/TokenizerStuff/Token.cs
@@ -0,0 +1,22 @@
+﻿namespace BasicMathExpressionParser.TokenizerStuff
+{
+    internal enum TokenType
+    {
+        Number,
+        Plus,
+        Dash,
+        Asterix,
+        Divide,
+    }
+
+    internal class Token
+    {
+        internal string Value { get; private set; }
+
+        internal TokenType Type { get; private set; }
+
+        internal Token(string value, TokenType type) => (Value, Type) = (value, type);
+
+        public override string ToString() => $"{Type}Token -> {Value}";
+    }
+}
