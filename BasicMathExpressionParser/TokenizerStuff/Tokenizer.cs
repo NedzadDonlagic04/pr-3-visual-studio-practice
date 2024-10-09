a93f64968c5843685ba88d12b1ac0e98c6fc7d25
NedzadDonlagic04 <nedzaddonlagic04@gmail.com>
2024-10-09 11:53:32 +0200

    Implemented the Tokenize method

--- BasicMathExpressionParser/TokenizerStuff/Tokenizer.cs
+++ BasicMathExpressionParser/TokenizerStuff/Tokenizer.cs
@@ -1,4 +1,5 @@
 ﻿using System.Text;
+using System.Text.RegularExpressions;
 
 namespace BasicMathExpressionParser.TokenizerStuff
 {
@@ -8,6 +9,19 @@ namespace BasicMathExpressionParser.TokenizerStuff
 
         private StringBuilder _source;
 
+        private readonly Dictionary<string, TokenType?> _tokenPatterns = new()
+        {
+            { @"^\d+(.\d+)?", TokenType.Number },
+            { @"^\+", TokenType.Plus },
+            { @"^-", TokenType.Dash },
+            { @"^\*", TokenType.Asterix },
+            { @"^\/", TokenType.Divide },
+
+            // Patterns below have a null value for TokenType
+            // Because these patterns are intended to just be eaten
+            { @"^\s+", null },
+        };
+
         internal Tokenizer(string source) => _source = new(source);
 
         public void ResetTokenizer(string source)
@@ -16,6 +30,28 @@ namespace BasicMathExpressionParser.TokenizerStuff
             _tokens.Clear();
         }
 
-        public List<Token> Tokenize() => new();
+        public List<Token> Tokenize()
+        {
+            while (_source.Length != 0)
+            {
+                foreach (var (pattern, tokenType) in _tokenPatterns)
+                {
+                    var matchResult = Regex.Match(_source.ToString(), pattern);
+
+                    if (!matchResult.Success)
+                    {
+                        continue;
+                    }
+                    else if (tokenType != null)
+                    {
+                        _tokens.Add(new(matchResult.Value, (TokenType)tokenType));
+                    }
+
+                    _source.Remove(0, matchResult.Value.Length);
+                }
+            }
+
+            return _tokens;
+        }
     }
 }
