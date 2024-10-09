59354e8229a2fb74c7aae1cd5d2d1e2490b527b8
NedzadDonlagic04 <nedzaddonlagic04@gmail.com>
2024-10-09 12:30:10 +0200

    Put exceptions in special folder

---
+++ BasicMathExpressionParser/TokenizerStuff/Exceptions/TokenizerException.cs
@@ -0,0 +1,11 @@
+﻿namespace BasicMathExpressionParser.TokenizerStuff.Exceptions
+{
+    internal class TokenizerException : Exception
+    {
+        internal TokenizerException() { }
+
+        internal TokenizerException(string message) : base(message) { }
+
+        internal TokenizerException(string message, Exception inner) : base(message, inner) { }
+    }
+}
---
+++ BasicMathExpressionParser/TokenizerStuff/Exceptions/TokenizerUnhandledPatternException.cs
@@ -0,0 +1,11 @@
+﻿namespace BasicMathExpressionParser.TokenizerStuff.Exceptions
+{
+    internal class TokenizerUnhandledPatternException : TokenizerException
+    {
+        internal TokenizerUnhandledPatternException() { }
+        
+        internal TokenizerUnhandledPatternException(string message) : base(message) { }
+        
+        internal TokenizerUnhandledPatternException(string message, Exception inner) : base(message, inner) { }
+    }
+}
