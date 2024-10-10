c80635aa701484c53687934c43ef0c33b5d8137c
NedzadDonlagic04 <nedzaddonlagic04@gmail.com>
2024-10-10 22:09:07 +0200

    Added IParser interface and implemented it with the Parser class

---
+++ BasicMathExpressionParser/ParserStuff/Classes/Parser.cs
@@ -0,0 +1,14 @@
+﻿using BasicMathExpressionParser.ParserStuff.Interfaces;
+
+using BasicMathExpressionParser.TokenizerStuff.Classes;
+using BasicMathExpressionParser.TokenizerStuff.Interfaces;
+
+namespace BasicMathExpressionParser.ParserStuff.Classes
+{
+    internal class Parser : IParser
+    {
+        private readonly Tokenizer _tokenizer = new();
+
+        public Expression Parse(string source) => new(new NumberExpression(5));
+    }
+}
---
+++ BasicMathExpressionParser/ParserStuff/Interfaces/IParser.cs
@@ -0,0 +1,9 @@
+﻿using BasicMathExpressionParser.ParserStuff.Classes;
+
+namespace BasicMathExpressionParser.ParserStuff.Interfaces
+{
+    internal interface IParser
+    {
+        public Expression Parse(string source);
+    }
+}
