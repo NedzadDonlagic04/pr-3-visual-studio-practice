8593ebb23692d17fdc32d49816bffa0826f8c1be
NedzadDonlagic04 <nedzaddonlagic04@gmail.com>
2024-10-10 22:08:25 +0200

    Added enum to represent the binding power of tokens

---
+++ BasicMathExpressionParser/ParserStuff/Enums/TokenBindingPower.cs
@@ -0,0 +1,12 @@
+﻿namespace BasicMathExpressionParser.ParserStuff.Enums
+{
+    internal enum TokenBindingPower : byte
+    {
+        Minimum,
+        Literal,        // Stuff like Number, if string literals ever get added could use this same thing
+        Additive,
+        Multiplicative,
+        Parenthesis,
+        Maximum,
+    }
+}
