369266bcde65be41963ee008e2c17c6be2bcdd00
NedzadDonlagic04 <nedzaddonlagic04@gmail.com>
2024-10-10 22:05:49 +0200

    Added comments

--- BasicMathExpressionParser/Extensions/Dictionary.cs
+++ BasicMathExpressionParser/Extensions/Dictionary.cs
@@ -2,6 +2,9 @@
 
 namespace BasicMathExpressionParser.Extensions
 {
+    // I know this exists in C# 7.0 but since I was using 6.0 thought it would be
+    // nice practice to make it myself, this the docs below are a nice source
+    // https://learn.microsoft.com/en-us/dotnet/api/system.collections.generic.collectionextensions.asreadonly?view=net-8.0
     internal static class Dictionary
     {
         internal static ReadOnlyDictionary<TKey, TValue> AsReadOnly<TKey, TValue>(
