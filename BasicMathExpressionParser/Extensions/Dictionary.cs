70f029cb272edad073f017f2f138a3cff1aa4ffe
NedzadDonlagic04 <nedzaddonlagic04@gmail.com>
2024-10-10 13:43:13 +0200

    Renamed file

--- BasicMathExpressionParser/Extensions/Dictionary.cs
+++ BasicMathExpressionParser/Extensions/Dictionary.cs
@@ -2,7 +2,7 @@
 
 namespace BasicMathExpressionParser.Extensions
 {
-    internal static class DictionaryExtensions
+    internal static class Dictionary
     {
         internal static ReadOnlyDictionary<TKey, TValue> AsReadOnly<TKey, TValue>(
             this Dictionary<TKey, TValue> dictionary
