aee54a4bee19b33cf7dab9ec746d723fe71795a2
NedzadDonlagic04 <nedzaddonlagic04@gmail.com>
2024-10-11 11:00:07 +0200

    Added exceptions for the Parser

---
+++ BasicMathExpressionParser/ParserStuff/Exceptions/ParserException.cs
@@ -0,0 +1,11 @@
+﻿namespace BasicMathExpressionParser.ParserStuff.Exceptions
+{
+    internal class ParserException : Exception
+    {
+        internal ParserException() { }
+        
+        internal ParserException(string message) : base(message) { }
+        
+        internal ParserException(string message, Exception inner) : base(message, inner) { }
+    }
+}
---
+++ BasicMathExpressionParser/ParserStuff/Exceptions/ParsingExpressionEmptyException.cs
@@ -0,0 +1,11 @@
+﻿namespace BasicMathExpressionParser.ParserStuff.Exceptions
+{
+    internal class ParsingExpressionEmptyException : ParserException
+    {
+        internal ParsingExpressionEmptyException() { }
+
+        internal ParsingExpressionEmptyException(string message) : base(message) { }
+
+        internal ParsingExpressionEmptyException(string message, Exception inner) : base(message, inner) { }
+    }
+}
