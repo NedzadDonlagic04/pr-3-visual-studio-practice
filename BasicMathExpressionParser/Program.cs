53925eea9f6e99fe1728f5b7b7dcf3d61251f020
NedzadDonlagic04 <nedzaddonlagic04@gmail.com>
2024-10-13 23:23:50 +0200

    Made main a bit more presentable

--- BasicMathExpressionParser/Program.cs
+++ BasicMathExpressionParser/Program.cs
@@ -4,15 +4,50 @@ namespace BasicMathExpressionParser
 {
     internal class Program
     {
+        private static readonly Parser _parser = new();
+
+        private static readonly string[] _validExitingProgramInputs = new string[] { "exit", "quit" };
+
+        private static void ParseMathExpression(string mathExpressionStr)
+        {
+            try
+            {
+                Expression expression = _parser.Parse(mathExpressionStr);
+                Console.WriteLine($"Result of evaluation the expression -> {expression.Eval()}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine();
+            }
+        }
+
         internal static void Main()
         {
-            Console.Write("Enter a basic math expression: ");
-            string mathExpression = Console.ReadLine() ?? "";
+            Console.WriteLine("If you wish to stop the execution of the program enter one "
+             + "of the following words (all uppercase or lowercase doesn't matter):");
+            foreach(var validExitProgramInput in _validExitingProgramInputs)
+            {
+                Console.WriteLine($"- {validExitProgramInput}");
+            }
+            Console.WriteLine();
 
-            Parser parser = new();
-            Expression expression = parser.Parse(mathExpression);
+            while (true)
+            {
+                Console.Write("Enter a basic math expression: ");
+                string input = Console.ReadLine() ?? "";
 
-            Console.WriteLine($"Result of evaluation the expression -> {expression.Eval()}");
+                if (_validExitingProgramInputs.Contains(input.ToLower()))
+                {
+                    break;
+                }
+
+                ParseMathExpression(input);
+            }
+                
         }
     }
 }
