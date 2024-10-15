4c8881de46b9c82f0b110d70230dfcc9792a9bfc
NedzadDonlagic04 <nedzaddonlagic04@gmail.com>
2024-10-15 09:46:36 +0200

    Made a test class to the Tokenizer and one test method

--- BasicMathExpressionParserTests/TokenizerTests.cs
+++ BasicMathExpressionParserTests/TokenizerTests.cs
@@ -1,12 +1,40 @@
+using BasicMathExpressionParser.TokenizerStuff.Classes;
+using BasicMathExpressionParser.TokenizerStuff.Enums;
+using BasicMathExpressionParser.TokenizerStuff.Interfaces;
+using NUnit.Framework;
+using Assert = NUnit.Framework.Assert;
+
 namespace BasicMathExpressionParserTests
 {
     [TestClass]
     public class TokenizerTests
     {
-        [TestMethod]
-        public void TestMethod1()
-        {
+        private readonly ITokenizer _tokenizer = new Tokenizer();
 
+        [TestMethod]
+        public void Tokenize_WithALotOfSpaces()
+        {
+            // Arrange
+            Queue<Token> expectedTokens = new();
+            expectedTokens.Enqueue(new Token("3", TokenType.Number));
+            expectedTokens.Enqueue(new Token("*", TokenType.Asterix));
+            expectedTokens.Enqueue(new Token("(", TokenType.OpenParenthesis));
+            expectedTokens.Enqueue(new Token("-", TokenType.Dash));
+            expectedTokens.Enqueue(new Token("4", TokenType.Number));
+            expectedTokens.Enqueue(new Token(")", TokenType.CloseParenthesis));
+            expectedTokens.Enqueue(new Token("+", TokenType.Plus));
+            expectedTokens.Enqueue(new Token("(", TokenType.OpenParenthesis));
+            expectedTokens.Enqueue(new Token("5", TokenType.Number));
+            expectedTokens.Enqueue(new Token("*", TokenType.Asterix));
+            expectedTokens.Enqueue(new Token("12", TokenType.Number));
+            expectedTokens.Enqueue(new Token(")", TokenType.CloseParenthesis));
+            expectedTokens.Enqueue(new Token("", TokenType.EndOfExpression));
+
+            // Act
+            var actualTokens = _tokenizer.Tokenize(" 3    * (  - 4 ) + (5 * 12)   ");
+
+            // Assert
+            Assert.That(actualTokens, Is.EqualTo(expectedTokens));
         }
     }
 }
