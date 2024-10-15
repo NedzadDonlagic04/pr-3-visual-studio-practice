6cae832326b4dd664cc4c46c7d1cbcd00e378654
NedzadDonlagic04 <nedzaddonlagic04@gmail.com>
2024-10-15 22:51:01 +0200

    Removed unnecessary newline

--- BasicMathExpressionParser/ParserStuff/Enums/TokenBindingPower.cs
+++ BasicMathExpressionParser/ParserStuff/Enums/TokenBindingPower.cs
@@ -6,6 +6,7 @@
         Literal,        // Stuff like Number, if string literals ever get added could use this same thing
         Additive,
         Multiplicative,
+        Exponential,
         Parenthesis,
         Maximum,
     }
