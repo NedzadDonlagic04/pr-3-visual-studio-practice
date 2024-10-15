0bba9d810a2f31709da1d65136a4eb59e914a423
NedzadDonlagic04 <nedzaddonlagic04@gmail.com>
2024-10-15 12:46:18 +0200

    Added exception throwing and bigger expression tests for Tokenizer

--- BasicMathExpressionParserTests/TokenizerTests.cs
+++ BasicMathExpressionParserTests/TokenizerTests.cs
@@ -1,5 +1,6 @@
 using BasicMathExpressionParser.TokenizerStuff.Classes;
 using BasicMathExpressionParser.TokenizerStuff.Enums;
+using BasicMathExpressionParser.TokenizerStuff.Exceptions;
 using BasicMathExpressionParser.TokenizerStuff.Interfaces;
 using NUnit.Framework;
 using Assert = NUnit.Framework.Assert;
@@ -35,6 +36,47 @@ namespace BasicMathExpressionParserTests
 
             // Assert
             Assert.That(actualTokens, Is.EqualTo(expectedTokens));
+        }
+
+        [TestMethod]
+        public void Tokenize_ExpressionWithAllAvailablePatterns()
+        {
+            // Arrange
+            Queue<Token> expectedTokens = new();
+            expectedTokens.Enqueue(new Token("(", TokenType.OpenParenthesis));
+            expectedTokens.Enqueue(new Token("(", TokenType.OpenParenthesis));
+            expectedTokens.Enqueue(new Token("11", TokenType.Number));
+            expectedTokens.Enqueue(new Token("/", TokenType.Divide));
+            expectedTokens.Enqueue(new Token("(", TokenType.OpenParenthesis));
+            expectedTokens.Enqueue(new Token("-", TokenType.Dash));
+            expectedTokens.Enqueue(new Token("1", TokenType.Number));
+            expectedTokens.Enqueue(new Token(")", TokenType.CloseParenthesis));
+            expectedTokens.Enqueue(new Token(")", TokenType.CloseParenthesis));
+            expectedTokens.Enqueue(new Token("+", TokenType.Plus));
+            expectedTokens.Enqueue(new Token("(", TokenType.OpenParenthesis));
+            expectedTokens.Enqueue(new Token("3", TokenType.Number));
+            expectedTokens.Enqueue(new Token("*", TokenType.Asterix));
+            expectedTokens.Enqueue(new Token("6", TokenType.Number));
+            expectedTokens.Enqueue(new Token(")", TokenType.CloseParenthesis));
+            expectedTokens.Enqueue(new Token(")", TokenType.CloseParenthesis));
+            expectedTokens.Enqueue(new Token("/", TokenType.Divide));
+            expectedTokens.Enqueue(new Token("+", TokenType.Plus));
+            expectedTokens.Enqueue(new Token("2", TokenType.Number));
+            expectedTokens.Enqueue(new Token("", TokenType.EndOfExpression));
+
+            // Act
+            var actualTokens = _tokenizer.Tokenize("((11 / (-1)) + (3 * 6)) / +2");
+
+            // Assert
+            Assert.That(actualTokens, Is.EqualTo(expectedTokens));
+        }
+
+        [TestMethod]
+        public void Tokenize_WhenPatternIsntValidToken_ShouldThrowTokenizerUnhandledPatternException()
+        {
+            // Assert
+            Assert.Throws<TokenizerUnhandledPatternException>(() => _tokenizer.Tokenize("5 + ? - 5"));
+            Assert.Throws<TokenizerUnhandledPatternException>(() => _tokenizer.Tokenize("2 * ! - # + 34"));
         }
     }
 }
