e708b95ba9919fc00eb6a9210f485ba1ae34b00b
NedzadDonlagic04 <nedzaddonlagic04@gmail.com>
2024-11-13 00:22:09 +0100

    Removed some unnecessary things from the minmax algorithm

--- WinFormsTicTacToe/Classes/HardTicTacToeBot.cs
+++ WinFormsTicTacToe/Classes/HardTicTacToeBot.cs
@@ -17,7 +17,7 @@ namespace WinFormsTicTacToe.Classes
             bool isPlayingX = ticTacToeBoard.Symbol == 'X';
 
             int moveIndex = -1;
-            int bestScore = int.MinValue;
+            int bestScore = isPlayingX ? int.MinValue : int.MaxValue;
             int oldBestScore = bestScore; 
 
             for (int i = 0; i < ticTacToeBoard.TotalSize; ++i)
@@ -31,14 +31,7 @@ namespace WinFormsTicTacToe.Classes
 
                 int score = GetBestMoveRecursive(ticTacToeBoard, !isPlayingX);
 
-                if (bestScore == int.MinValue)
-                {
-                    bestScore = score;
-                }
-                else
-                {
-                    bestScore = GetBestScoreForGivenPlayer(bestScore, score, isPlayingX);
-                }
+                bestScore = isPlayingX ? Math.Max(bestScore, score) : Math.Min(bestScore, score);
 
                 if (oldBestScore != bestScore)
                 {
@@ -57,66 +50,6 @@ namespace WinFormsTicTacToe.Classes
         #region Private methods
 
         /// <summary>
-        ///     For the given scores and player returns the best score
-        ///     for the given player
-        /// </summary>
-        /// <param name="score1"></param>
-        /// <param name="score2"></param>
-        /// <param name="isPlayingX"></param>
-        /// <returns>Returns the best score for the player</returns>
-        private static int GetBestScoreForGivenPlayer(int score1, int score2, bool isPlayingX)
-        {
-            /*
-             * A bit of an explanation as to what is happening here with
-             * the whacky condition.
-             * 
-             * Let's assume we're playing X
-             * If X gets a choice between scores 1 and 3 it wants to take 1 because 
-             * that means it's closer to a victory than 3. Because of the way it's
-             * calculated (at least as of the time of writing this comment) the 
-             * 3 result would mean that the depth to get to it was 3 moves while 
-             * 1 would mean it took a single move for victory.
-             * 
-             * But if it gets scores like -8 and -5 it wants to pick -8 because even
-             * if both imply loses for the player X it choosing the longer
-             * route would give the opponent a chance to mess up and not achieve
-             * victory.
-             * 
-             * That's why if it's the same sign we want to apply the Math.Min method
-             * If the signs are different, like -6 and 5, we want the Math.Max result.
-             * 
-             * This same logic can be applied to the player O just switch up
-             * the signs from the explanation above.
-             * 
-             * This could have been written in a single line or a bit less than what
-             * I ended up sticking with. I didn't do that because it looked uglier than
-             * the longer version.
-             */
-            bool isSameSign = (score1 < 0 && score2 < 0) || (score1 > 0 && score2 > 0);
-
-            if (isPlayingX)
-            {
-                if (isSameSign)
-                {
-                    return Math.Min(score1, score2);
-                }
-                else
-                {
-                    return Math.Max(score1, score2);
-                }
-            }
-
-            if (isSameSign)
-            {
-                return Math.Max(score1, score2);
-            }
-            else
-            {
-                return Math.Min(score1, score2);
-            }
-        }
-
-        /// <summary>
         ///     Searches for the best outcome through the use of 
         ///     recursion and a for loop
         /// </summary>
@@ -129,15 +62,13 @@ namespace WinFormsTicTacToe.Classes
         /// <inheritdoc cref="TicTacToeBoard.UndoLastSymbolPlace"/>
         private int GetBestMoveRecursive(TicTacToeBoard ticTacToeBoard, bool isPlayingX, int depth = 0)
         {
-            ++depth;
-
             TicTacToeGameState gameState = ticTacToeBoard.GameState;
             if (gameState != TicTacToeGameState.GameOngoing)
             {
-                return GetScoreForGameState(gameState) * depth;
+                return GetScoreForGameState(gameState, depth);
             }
 
-            int bestScore = int.MinValue;
+            int bestScore = isPlayingX? int.MinValue : int.MaxValue;
 
             for (int i = 0; i < ticTacToeBoard.TotalSize; ++i)
             {
@@ -150,14 +81,7 @@ namespace WinFormsTicTacToe.Classes
 
                 int score = GetBestMoveRecursive(ticTacToeBoard, !isPlayingX, depth + 1);
 
-                if (bestScore == int.MinValue)
-                {
-                    bestScore = score;
-                }
-                else
-                {
-                    bestScore = GetBestScoreForGivenPlayer(bestScore, score, isPlayingX);
-                }
+                bestScore = isPlayingX ? Math.Max(bestScore, score) : Math.Min(bestScore, score);
 
                 ticTacToeBoard.UndoLastSymbolPlace();
             }
@@ -173,10 +97,10 @@ namespace WinFormsTicTacToe.Classes
         /// <exception cref="NotImplementedException">
         ///     Thrown if a given game state is not implemented
         /// </exception>
-        private static int GetScoreForGameState(TicTacToeGameState gameState) => gameState switch
+        private static int GetScoreForGameState(TicTacToeGameState gameState, int depth) => gameState switch
         {
-            TicTacToeGameState.PlayerXWon => 1,
-            TicTacToeGameState.PlayerOWon => -1,
+            TicTacToeGameState.PlayerXWon => 10 - depth,
+            TicTacToeGameState.PlayerOWon => depth - 10,
             TicTacToeGameState.Tie => 0,
             _ => throw new NotImplementedException($"Game state \"{gameState}\" not implemented")
         };
