b13ef2bacf0b8f7535c259529dd562b9b0e337b7
NedzadDonlagic04 <nedzaddonlagic04@gmail.com>
2024-11-18 21:15:43 +0100

    Called tests from main

---
+++ Random-DSA-Stuff/Program.cs
@@ -0,0 +1,15 @@
+﻿using Random_DSA_Stuff.ADT.LinkedLists;
+using Random_DSA_Stuff.ADT_Tests;
+
+namespace Random_DSA_Stuff
+{
+    internal class Program
+    {
+
+        static void Main()
+        {
+            LinkedListTests<SinglyLinkedList<int>>.RunTests();
+            LinkedListTests<DoublyLinkedList<int>>.RunTests();
+        }
+    }
+}
