dcd134c35807a988cf546c75b0f2087774de4f40
NedzadDonlagic04 <nedzaddonlagic04@gmail.com>
2024-11-18 21:14:39 +0100

    Added extension to get type name in a nicer format

---
+++ Random-DSA-Stuff/Extensions/TypeExtensions.cs
@@ -0,0 +1,18 @@
+﻿namespace Random_DSA_Stuff.Extensions
+{
+    internal static class TypeExtensions
+    {
+        internal static string GetFormattedName(this Type type)
+        {
+            if (!type.IsGenericType)
+            {
+                return type.Name;
+            }
+
+            string genericName = type.Name[..type.Name.IndexOf('`')];
+            string genericArguments = string.Join(", ", type.GetGenericArguments().Select(GetFormattedName));
+
+            return $"{genericName}<{genericArguments}>";
+        }
+    }
+}
