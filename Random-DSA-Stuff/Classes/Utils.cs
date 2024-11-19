439c0318cb32b2d58479bceebdceb7a19ab66526
NedzadDonlagic04 <nedzaddonlagic04@gmail.com>
2024-11-19 10:12:55 +0100

    Added static utils class to hold some utility methods

---
+++ Random-DSA-Stuff/Classes/Utils.cs
@@ -0,0 +1,19 @@
+﻿namespace Random_DSA_Stuff.Classes
+{
+    internal static class Utils
+    {
+        #region Internal methods
+
+        internal static void PrintList<T>(string message, IEnumerable<T> list)
+        {
+            Console.WriteLine(message);
+            foreach (var num in list)
+            {
+                Console.Write($"{num} ");
+            }
+            Console.WriteLine();
+        }
+
+        #endregion
+    }
+}
