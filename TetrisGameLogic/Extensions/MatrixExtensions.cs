a67e5c05dca6b9a8b5795e8145e37a54a604189e
NedzadDonlagic04 <nedzaddonlagic04@gmail.com>
2025-02-07 00:51:37 +0100

    Added matrix extension class

---
+++ TetrisGameLogic/Extensions/MatrixExtensions.cs
@@ -0,0 +1,22 @@
+﻿namespace TetrisGameLogic.Extensions
+{
+    public static class MatrixExtensions
+    {
+        #region Public methods
+
+        public static MatrixType[,] FillWith<MatrixType>(this MatrixType[,] matrix, MatrixType value)
+        {
+            for (int i = 0; i < matrix.GetLength(0); ++i)
+            {
+                for (int ii = 0; ii < matrix.GetLength(1); ++ii)
+                {
+                    matrix[i, ii] = value;
+                }
+            }
+
+            return matrix;
+        }
+
+        #endregion
+    }
+}
