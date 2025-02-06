1adc19d7a5afec1cf539d0efa89ae01156d381c3
NedzadDonlagic04 <nedzaddonlagic04@gmail.com>
2025-02-07 00:51:57 +0100

    Added empty class to represent game arena

---
+++ TetrisGameLogic/Classes/TetrisGameArena.cs
@@ -0,0 +1,17 @@
+﻿namespace TetrisGameLogic.Classes
+{
+    public class TetrisGameArena
+    {
+        #region Fields, properties and constants
+
+        #endregion
+
+        #region Public methods
+
+        #endregion
+
+        #region Private methods
+
+        #endregion
+    }
+}
