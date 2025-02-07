d0151c8b1089e145dabf303b974e5429934fc085
NedzadDonlagic04 <nedzaddonlagic04@gmail.com>
2025-02-07 12:19:59 +0100

    Added class to display the game board for tetris

---
+++ TerminalTetris/TetrisGameDisplay.cs
@@ -0,0 +1,20 @@
+﻿using TetrisGameLogic.Classes;
+
+namespace TerminalTetris
+{
+    public class TetrisGameDisplay
+    {
+        #region Public methods
+
+        public void DrawGameBoard(TetrisGame tetrisGame)
+        {
+
+        }
+
+        #endregion
+
+        #region Private methods
+
+        #endregion
+    }
+}
