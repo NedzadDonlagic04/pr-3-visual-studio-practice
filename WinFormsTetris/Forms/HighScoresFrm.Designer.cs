027530fb0e30b95a22e42823b3efee6925f4949e
NedzadDonlagic04 <nedzaddonlagic04@gmail.com>
2025-02-20 22:49:01 +0100

    Finished high scores form

--- WinFormsTetris/Forms/HighScoresFrm.Designer.cs
+++ WinFormsTetris/Forms/HighScoresFrm.Designer.cs
@@ -28,19 +28,96 @@
         /// </summary>
         private void InitializeComponent()
         {
+            highScoresLbl = new Label();
+            highScoresDgv = new DataGridView();
+            Pos = new DataGridViewTextBoxColumn();
+            Score = new DataGridViewTextBoxColumn();
+            LinesCleared = new DataGridViewTextBoxColumn();
+            DateAndTime = new DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)highScoresDgv).BeginInit();
             SuspendLayout();
+            // 
+            // highScoresLbl
+            // 
+            highScoresLbl.AutoSize = true;
+            highScoresLbl.BackColor = Color.Transparent;
+            highScoresLbl.Font = new Font("Segoe UI", 48F, FontStyle.Bold);
+            highScoresLbl.ForeColor = Color.WhiteSmoke;
+            highScoresLbl.Location = new Point(155, 28);
+            highScoresLbl.Name = "highScoresLbl";
+            highScoresLbl.Size = new Size(491, 106);
+            highScoresLbl.TabIndex = 4;
+            highScoresLbl.Text = "High Scores";
+            highScoresLbl.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // highScoresDgv
+            // 
+            highScoresDgv.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            highScoresDgv.Columns.AddRange(new DataGridViewColumn[] { Pos, Score, LinesCleared, DateAndTime });
+            highScoresDgv.Location = new Point(12, 148);
+            highScoresDgv.Name = "highScoresDgv";
+            highScoresDgv.RowHeadersWidth = 51;
+            highScoresDgv.ScrollBars = ScrollBars.Horizontal;
+            highScoresDgv.Size = new Size(776, 316);
+            highScoresDgv.TabIndex = 5;
+            highScoresDgv.SelectionChanged += HighScoresDgvSelectionChanged;
+            // 
+            // Pos
+            // 
+            Pos.DataPropertyName = "Pos";
+            Pos.HeaderText = "";
+            Pos.MinimumWidth = 6;
+            Pos.Name = "Pos";
+            Pos.Width = 125;
+            // 
+            // Score
+            // 
+            Score.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            Score.DataPropertyName = "Score";
+            Score.HeaderText = "Score";
+            Score.MinimumWidth = 6;
+            Score.Name = "Score";
+            // 
+            // LinesCleared
+            // 
+            LinesCleared.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            LinesCleared.DataPropertyName = "LinesCleared";
+            LinesCleared.HeaderText = "Lines cleared";
+            LinesCleared.MinimumWidth = 6;
+            LinesCleared.Name = "LinesCleared";
+            // 
+            // DateAndTime
+            // 
+            DateAndTime.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            DateAndTime.DataPropertyName = "DateTime";
+            DateAndTime.HeaderText = "Date and time";
+            DateAndTime.MinimumWidth = 6;
+            DateAndTime.Name = "DateAndTime";
             // 
             // HighScoresFrm
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            BackgroundImage = Resource.tetris_bg_purple;
+            ClientSize = new Size(800, 476);
+            Controls.Add(highScoresDgv);
+            Controls.Add(highScoresLbl);
             Name = "HighScoresFrm";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "High Scores";
+            Load += HighScoresFormLoad;
+            ((System.ComponentModel.ISupportInitialize)highScoresDgv).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label highScoresLbl;
+        private DataGridView highScoresDgv;
+        private DataGridViewTextBoxColumn Pos;
+        private DataGridViewTextBoxColumn Score;
+        private DataGridViewTextBoxColumn LinesCleared;
+        private DataGridViewTextBoxColumn DateAndTime;
     }
 }
--- WinFormsTetris/Forms/HighScoresFrm.cs
+++ WinFormsTetris/Forms/HighScoresFrm.cs
@@ -1,8 +1,12 @@
-﻿namespace WinFormsTetris.Forms
+﻿using TetrisGameLogic.Classes;
+
+namespace WinFormsTetris.Forms
 {
     public partial class HighScoresFrm : Form
     {
         #region Fields and properties
+
+        private readonly HighScoreManager _highScoreManager = new();
 
         #endregion
 
@@ -16,6 +20,42 @@
         #endregion
 
         #region Private methods
+
+        private void HighScoresFormLoad(object? sender, EventArgs e)
+        {
+            SetDgvProperties();
+            LoadHighScoresIntoDgv();
+        }
+
+        private void HighScoresDgvSelectionChanged(object? sender, EventArgs e)
+        {
+            highScoresDgv.ClearSelection();
+        }
+
+        private void SetDgvProperties()
+        {
+            highScoresDgv.ReadOnly = true;
+            highScoresDgv.MultiSelect = false;
+            highScoresDgv.AutoGenerateColumns = false;
+            highScoresDgv.AllowUserToResizeColumns = false;
+            highScoresDgv.AllowUserToResizeRows = false;
+        }
+
+        private void LoadHighScoresIntoDgv()
+        {
+            var highScores = _highScoreManager.HighScores;
+
+            highScoresDgv.DataSource = highScores;
+
+            for (int i = 0; i < highScores.Count; ++i)
+            {
+                highScoresDgv.Rows[i].Cells["Pos"].Value = i + 1;
+                if ((DateTime.Now - highScores[i].DateTime).Minutes <= 5)
+                {
+                    highScoresDgv.Rows[i].DefaultCellStyle.BackColor = Color.LightGreen;
+                }
+            }
+        }
 
         #endregion
     }
